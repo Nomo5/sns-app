2fbb5da5d66efab980b41a3453195fd490da28de
Nomo5 <106419953+Nomo5@users.noreply.github.com>
2022-12-26 13:15:40 +0900

    Update env.php

--- php/env.php
+++ php/env.php
@@ -2,8 +2,8 @@
 
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'sns');
-define('DB_USER', 'nomo');
-define('DB_PASS', 'secret');
+define('DB_USER', '*******');
+define('DB_PASS', '*******');
 
 
 ?>
